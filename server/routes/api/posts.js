57d4ce4e352ce1271932ede05985823bd9e8f5c7
Amir <101679935+amir-aliyu@users.noreply.github.com>
2024-04-22 00:09:08 -0400

    routes all work

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,23 +1,24 @@
 const express = require('express');
+const mongodb = require('mongodb');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const router = express.Router();
 
+const bodyParser = require('body-parser');
+// Add this line before defining routes
+router.use(bodyParser.json());
 // Get Posts
+/*
 router.get('/', async (req, res) => {
   try {
-      const db = client.db("Cluster0"); // Replace "your_database_name" with your actual database name
+      const db = client.db("Cluster0");
       const posts = await db.collection("posts").find({}).toArray(); // Retrieve all posts from the "posts" collection
       res.json(posts); // Send the posts as JSON response
   } catch (error) {
       console.error("Error retrieving posts:", error);
       res.status(500).json({ message: "Internal server error" });
   }
-});
+});*/
 
-// Add Post
-
-
-// Delete Post
 
 // connect to post collection
 const uri = "mongodb+srv://amir:amiri@cluster0.xtqec0u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -55,8 +56,24 @@ async function defineRouteHandlers() {
           }
       });
 
-      // Add more route handlers if needed
+      // post request
+      router.post('/', async (req, res) => {
+        const db = client.db("Cluster0");
+        const posts = await db.collection("posts").insertOne({
+          text:req.body.text,
+          createdAt: new Date()
+          // maybe res.json(posts) this? 
+        });
+        res.status(201).send(); // this says everythings ok but something changed
+      });
 
+      // delete request
+      router.delete('/:id', async (req, res) => {
+        const db = client.db("Cluster0");
+        const posts = await db.collection("posts").deleteOne({_id: new mongodb.ObjectId(req.params.id)});
+        res.status(200).send();
+      });
+      
   } catch (err) {
       console.error("Failed to define route handlers:", err);
   }
