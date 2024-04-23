f598e404598f05ddedbe9bdd86f0c20e497bd35a
Amir <101679935+amir-aliyu@users.noreply.github.com>
2024-04-22 22:26:57 -0400

    deleting vuetify

--- client/src/main.js
+++ client/src/main.js
@@ -1,17 +1,25 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { createVuetify } from 'vuetify'
-import App from './App.vue'
+//import { createApp } from 'vue'
+//import { createPinia } from 'pinia'
+//import { createVuetify } from 'vuetify'
+//import App from './App.vue'
 
 //import './assets/main.css'
 
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import 'vuetify/styles'
+//import * as components from 'vuetify/components'
+//import * as directives from 'vuetify/directives'
+//import 'vuetify/styles'
 
-const app = createApp(App)
-app.use(createPinia()).use(createVuetify({
-    components,
-    directives
-})).mount('#app')
+//const app = createApp(App)
+//app.use(createPinia()).use(createVuetify({
+ //   components,
+   // directives
+//})).mount('#app')
 
+import { createApp } from 'vue'; // Import createApp function from Vue
+import App from './App.vue'; // Import the root component
+
+// Create a new Vue application instance
+const app = createApp(App);
+
+// Mount the root component to the DOM
+app.mount('#app');
